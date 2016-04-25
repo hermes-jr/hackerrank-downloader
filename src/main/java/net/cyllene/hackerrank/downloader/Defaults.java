6bf107b47b1c3c65bf731f390e5e9bc20bb9572d
Mikhail Antonov <hermes@compot.ru>
2016-04-26 02:30:20 +0300

    Added full jar assembly task to build.gradle

--- src/main/java/net/cyllene/hackerrank/downloader/Defaults.java
+++ src/main/java/net/cyllene/hackerrank/downloader/Defaults.java
@@ -1,6 +1,11 @@
 package net.cyllene.hackerrank.downloader;
 
 class Defaults {
+	// Suppress default constructor for noninstantiability
+	private Defaults() {
+		throw new AssertionError();
+	}
+
 	static final String DOMAIN = "www.hackerrank.com";
 	static final String HOST = "https://" + DOMAIN;
 	static final String SECRET_COOKIE_ID = "_hackerrank_session";
