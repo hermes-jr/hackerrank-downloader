c8a4a95df8ee2c736c69bbb4a727dbeee312fb20
Mikhail Antonov <hermes@compot.ru>
2016-04-28 23:38:51 +0300

    gradle configuration changed, all tests should work in non-utf environment product is under ASL now

--- src/main/java/net/cyllene/hackerrank/downloader/DownloaderCore.java
+++ src/main/java/net/cyllene/hackerrank/downloader/DownloaderCore.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2016 Mikhail Antonov
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.cyllene.hackerrank.downloader;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -57,7 +72,7 @@ enum DownloaderCore {
 	/**
 	 * Provides a way to inject mock {@link HttpClient}
 	 *
-	 * @param client
+	 * @param client {@link HttpClient} may be injected via this method
 	 */
 	public void setHttpClient(HttpClient client) {
 		httpClient = client;
